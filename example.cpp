8bff6e329ba57e37e7c880abead82b58b823012c
Mohamed Abdelkawy <19490854+mabdelkawy@users.noreply.github.com>
2019-10-25 20:51:42 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -13,6 +13,7 @@ int main()
     //start_range is range of N to start from then automatically incremental (N+1,N+2,..)
   
   string stopped_at="ss";        //last string at where you were intrupted
+                                 //if not intrupted use the overloaded function of ribasen
   start_range=stopped_at.size(); //setting range from where you stopped
   mode=began;                    //mode is mode if you generate from normal begining (began) 
                                  //or continue from intrupted one (continued)
